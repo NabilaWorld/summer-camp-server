f146f358f176cb07e3a0974e45b6ec2cc9249f8c
NabilaWorld <mnabila30163@gmail.com>
2023-06-09 19:44:13 +0600

    convert users as a admin

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zx1pf4.mongodb.net/?retryWrites=true&w=majority`;
@@ -105,17 +120,17 @@ async function run() {
 
 
     // cart collection apis
-    app.get('/carts',  async (req, res) => {
+    app.get('/carts', verifyJWT,  async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
       }
 
-      // const decodedEmail = req.decoded.email;
-      // if (email !== decodedEmail) {
-      //   return res.status(403).send({ error: true, message: 'porviden access' })
-      // }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'porbiden access' })
+      }
 
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
